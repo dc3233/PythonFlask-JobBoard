61e94f606909154f24e56f57407d748e8994229f
Tommy Keen <tommy.keen@gmail.com>
2019-06-27 13:37:58 -0400

    is true

--- jobs/app.py
+++ jobs/app.py
@@ -18,11 +18,10 @@ def open_connection():
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection
     cursor = connection.execute(sql, values)
-    if commit ==True:
+    if commit is True:
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
-    
     cursor.close()
     return results
 
