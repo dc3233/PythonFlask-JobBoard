b04be9dc3654725fc72ec949583003b2520c8b5f
davidcheng <chopin914@gmail.com>
2020-05-23 23:32:11 -0400

    Completed module

--- tests/test_module5.py
+++ tests/test_module5.py
@@ -33,7 +33,7 @@ def test_app_job_route_parameter_module5():
 @pytest.mark.test_app_job_route_data_module5
 def test_app_job_route_data_module5():
     assert 'job' in dir(app), 'Have you created the `job` function?'
-    execute_sql = 'execute_sql:SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?:job_id:single:True'
+    # execute_sql = 'execute_sql:SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?:job_id:single:True'
     # assert execute_sql in get_functions(app.job), '`execute_sql` has not been called or has the wrong parameters.'
 
 @pytest.mark.test_app_job_route_pass_data_module5
